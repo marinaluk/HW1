42f128e396179ddedcf7dbc7d3b601eb31cd234e
unknown <C:\Users\Marina\AppData\Roaming\The Bat!>
2022-11-26 00:46:28 +0800

    сделали задачу 3

---
+++ homw1/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("input number:");
+int num = Convert.ToInt32(Console.ReadLine());
+    if (num%2==0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
