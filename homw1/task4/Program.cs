5ebaed68c33cba6c3ea8b1bd8fea1f149de5c409
unknown <C:\Users\Marina\AppData\Roaming\The Bat!>
2022-11-26 02:12:40 +0800

    добавили 4 задачу, но что то не получилось

---
+++ homw1/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("input number N:");
+int N = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < N; i++)
+    {
+    Console.Write("Введите элемент {0}: " + i.ToString()); 
+    if (int.Parse(Console.ReadLine("N"% 2 == 0))
+    ++("count"));
+    }
+
+Console.WriteLine("Количество нечетных элементов: " + count.ToString());
